346f1e48319adc9f888fb56abe0668bd533b2db1
miabu-pashh <mdmaibubpasha@gmail.com>
2024-10-22 19:22:25 -0500

    Refactor router basename in App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Project1">
       <div className="App">
         <TitleHeader />
         <div className="page-container">
